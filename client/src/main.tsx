be9a758dfb9de65bcf3757b1f140a477120a5927
Oleg Ilin <olegilin@MacBook-Air-Oleg.local>
2025-04-06 12:52:37 +0300

    update stylelint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./Global.module.scss";
 import { App } from "./App.tsx";
 import store from "./store";
 import { Provider } from "react-redux";
